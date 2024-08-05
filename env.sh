34c72c11c5544e4c0db05651154baf2b70f8bad1
uynlecloud@gmail.com <uynlecloud@gmail.com>
2024-08-05 10:54:41 +0700

    update git

--- env.sh
+++ env.sh
@@ -11,3 +11,12 @@ if ! command -v git &> /dev/null; then
 else
   echo "Git is already installed on this system.❎❎❎"
 fi
+# === User Information ===
+GIT_USER_NAME="UynLe"       # Replace with your actual name
+GIT_USER_EMAIL="uynle76@gmail.com"  # Replace with your actual email
+
+# === Git Configuration ===
+git config --global user.name "$GIT_USER_NAME"
+git config --global user.email "$GIT_USER_EMAIL"
+
+echo "Git user name and email configured successfully!"
