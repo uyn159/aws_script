e6762b8b405a11170ca7bcb5c894565a46c3b7ef
uynlecloud@gmail.com <uynlecloud@gmail.com>
2024-08-05 16:13:19 +0700

    update

--- install_docker.sh
+++ install_docker.sh
@@ -57,3 +57,4 @@ fi
 # Fix permission
 echo "Fix:'Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock'"
 newgrp docker
+chmod 666 /var/run/docker.sock
